6875744297ab85ee8000e2663cb9b891c2756515
Craig Bilner <craig.bilner@gmail.com>
2017-06-02 15:44:29 +0100

    Import card component

--- src/components/app.js
+++ src/components/app.js
@@ -1,30 +1,9 @@
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import Card from '@lal/card';
 
-const Card = ({ children }) => <View style={styles.card}>{children}</View>;
-const Title = ({ children }) => <Text style={styles.title}>{children}</Text>;
-const Photo = ({ uri }) => <Image source={{ uri }} style={styles.image}/>;
-const App = () => (
-  <Card>
-    <Title>Some sort of property</Title>
-    <Photo uri="https://lid.zoocdn.com/645/430/14b6a430b76a235e8de2ba02d695e7a0bd789b40.jpg"/>
-  </Card>
-);
-
-const styles = StyleSheet.create({
-  card: {
-    flexGrow: 1,
-    justifyContent: 'center'
-  },
-  title: {
-    fontSize: '1.25rem',
-    fontWeight: 'bold'
-  },
-  image: {
-    height: 400,
-    marginVertical: 10,
-    width: 400
-  }
-});
+const App = () =>
+  <Card title="Some sort of property"
+        uri="https://lid.zoocdn.com/645/430/14b6a430b76a235e8de2ba02d695e7a0bd789b40.jpg"
+  />;
 
 export default App;
